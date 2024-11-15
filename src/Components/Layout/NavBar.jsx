293b3a2229c1a135d0b7c65d58227067d9ecbc17
imad elkhettab <elkhettab_imad@emsi-edu.ma>
2024-11-15 09:27:55 +0100

    install

--- src/Components/Layout/NavBar.jsx
+++ src/Components/Layout/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { NavLink, Link, useNavigate } from "react-router-dom";
-import Logout from "../auth/Logout";
+import Logout from "../Auth/Logout";
 
 const NavBar = () => {
     const [showAccount, setShowAccount] = useState(false);
