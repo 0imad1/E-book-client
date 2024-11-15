87f5c7a35dc86343ea3decfc4cd7956ced021bc7
imad elkhettab <elkhettab_imad@emsi-edu.ma>
2024-11-15 13:48:35 +0100

    xml

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteSitemap from 'vite-plugin-sitemap';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    ViteSitemap({
+      siteUrl: 'https://pdfversee.netlify.app',
+      
+      // Optional settings for the sitemap
+      changefreq: 'daily', // Frequency of content changes
+      priority: 0.7,       // Default priority (between 0.0 and 1.0)
+      
+      // Optional: You can specify paths to include or exclude
+      exclude: [ '/GJWQ'], // Pages you don’t want to include in the sitemap
+    }),
+  ],
+ 
+  
+   build: {
+    // Output directory for production build
+    outDir: 'dist',
+  },
 })
